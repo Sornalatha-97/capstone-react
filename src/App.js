d0bc3a607a180c9aaa59bf4de189fdb146979b24
Sorna Latha M <sornalatha@datayaan.com>
2023-11-11 20:24:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState, useRef } from "react";
+import Home from "../src/pages/Home/Home";
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
